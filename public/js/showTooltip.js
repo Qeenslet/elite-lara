2a3cea39ce24c7367e5ab393fd5657f78b1d3cfd
Qeenslet <egorgulidow@mail.ru>
2015-09-29 22:29:17 +0300

    style fixes

--- public/js/showTooltip.js
+++ public/js/showTooltip.js
@@ -1,14 +1,19 @@
 /**
  * Created by egorg_000 on 27.09.2015.
  */
+coord = $('#stats').offset();
 $('.extra').mouseover(function(eventObject)
     {
         data_tooltip = $(this).attr("data-tooltip");
-
+        scrolled = window.pageYOffset;
+        if (coord.top > scrolled)
+            shift = 0;
+        else
+            shift = scrolled - coord.top;
+        //if(firstmeasure > scrolled + 200) shift = 0;
         $("#tooltip").html(data_tooltip)
             .css({
-                "top" : eventObject.pageY - 350,
-                "left" : eventObject.pageX - 600
+                "top" : shift
             })
             .show();
     }).mouseout(function () {
@@ -16,7 +21,6 @@ $('.extra').mouseover(function(eventObject)
     $("#tooltip").hide()
         .html("")
         .css({
-            "top" : 0,
-            "left" : 0
+            "top" : 0
         })
 });
